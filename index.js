f2ae4964e340a99fd8a6d68d5f86ecb75f73c7a3
Yash-Thakkar-1209 <thakkaryash458@gmail.com>
2023-10-17 11:53:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+  strings: [
+    "FrontEnd Developer",
+    "Web Developer",
+    "React Developer",
+    "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
